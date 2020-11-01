e16616c791c271d1fce172dc15341b3659e78729
Muzappar <979697473@qq.com>
2020-11-01 23:11:18 +0800

    补充组件注释

--- miniprogram/js/framework/Component.js
+++ miniprogram/js/framework/Component.js
@@ -9,6 +9,7 @@ export default class Component {
   /**
    * 创建一个新的组件
    * @param {String} name 组件类型名称
+   * @param {Boolean} isUnique 该类组件是否唯一
    */
   constructor(name, isUnique) {
     this.Name = name
